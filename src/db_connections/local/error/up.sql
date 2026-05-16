414284ed4f2482daec61b0f4067d855d17ddbb4f
Pier Niccolò Giannelli <pierniccologiannelli@gmail.com>
2026-05-16 16:30:06 +0200

    chore: stop tracking local db connection head and error files

--- src/db_connections/local/error/up.sql
+++
@@ -1,9 +0,0 @@
-CREATE USER 'simba'@'localhost' IDENTIFIED BY 'pass';
-
-GRANT ALL PRIVILEGES ON `simba_app`.* TO 'simba'@'localhost' WITH GRANT OPTION;
-
-FLUSH PRIVILEGES;
-
-
-
--- SHOW GRANTS FOR 'simba'@'localhost';
